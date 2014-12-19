bc1d402ae8136b64b0a0779a59b8f1f23698d6c9
Valentin Thirion <valentinthirion@gmail.com>
2014-12-19 16:10:28 +0100

    First working version

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Deliveries labels printing",
+    'version': '1.0',
+    'depends': ['sale', 'report'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Stock',
+    'description': """
+    Print labels for deliveries.
+    """,
+    'data': [
+        'reports.xml',
+		'data.xml',
+    ],
+    'demo': [
+      'demo.xml',
+    ],
+}
