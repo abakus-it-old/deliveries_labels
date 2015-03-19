54f52753b1d912715b32d110fa8c99c9109cdd62
Valentin THIRION <valentinthirion@gmail.com>
2015-03-19 16:30:07 +0100

    [ADD] Usable from the stock picking list

--- report.py
+++ report.py
@@ -4,7 +4,12 @@ class sale_delivery_labels(models.Model):
     _inherit = ['sale.order']
 
     def print_labels(self, cr, uid, ids, context=None):
+        #report name: sale.report_deliverieslabels
+        return self.pool['report'].get_action(cr, uid, ids, 'sale.report_deliverieslabels', context=context)
+    
+class stock_delivery_labels(models.Model):
+    _inherit = ['stock.picking']
+
+    def print_labels(self, cr, uid, ids, context=None):
         #report name: stock.report_deliverieslabels
         return self.pool['report'].get_action(cr, uid, ids, 'stock.report_deliverieslabels', context=context)
-    
-
